a4b906fa6ee6969f6098d2f1a2863a9d916fd4ce
Petr5 <petr.mavzovin@mail.ru>
2023-03-07 18:46:17 +0300

    add class for data hh.ru data processing, change start message

---
+++ ProcessHhData.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import seaborn as sns
+
+
+class ProcessHhData:
+    @classmethod
+    def get_necessery_skills(message, vacancies):
+        vacancies_df = pd.json_normalize(vacancies)  # ????
+        data = pd.DataFrame(vacancies_df,
+                            columns=['id', 'name', 'description', 'key_skills', 'salary.from', 'salary.to',
+                                     'salary.currency', 'salary.gross', 'address.lat', 'address.lng',
+                                     'experience.id', 'employer.name',
+                                     ])
+
+        # data.to_excel(f"{message.from_id}_{message.message_id}.xlsx")
+
+        skills = {}  #
+        for i in range(len(data)):  # Подсчет скилов
+            for skill in data.iloc[i]['key_skills']:
+                if skill['name'] not in skills.keys():
+                    skills[skill['name']] = 0
+                skills[skill['name']] += 1
+
+        skills = pd.Series(skills).sort_values(ascending=False).head(15)
+        top_skills = pd.DataFrame(skills).T
+        plot = sns.barplot(top_skills, orient='h')
+        plot.set(title=f'Top 15 skills"{message.text}"')
+        fig = plot.get_figure()  # рисуем график со скиллами
+        fig.savefig(f"{message.from_id}_{message.message_id}.png", bbox_inches="tight")
--- bot.py
+++ bot.py
@@ -10,6 +10,7 @@ from tqdm import tqdm
 
 import texts
 from config import *
+from ProcessHhData import *
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -65,28 +66,8 @@ async def send_hh_search_query(message: types.Message):
         vacancies[index] = vacancy_info
         await sleep(0.9)
 
-    vacancies_df = pd.json_normalize(vacancies) # ????
-    data = pd.DataFrame(vacancies_df, columns=['id', 'name', 'description', 'key_skills', 'salary.from', 'salary.to',
-                                               'salary.currency', 'salary.gross', 'address.lat', 'address.lng',
-                                               'experience.id', 'employer.name',
-                                               ])
-
-    # data.to_excel(f"{message.from_id}_{message.message_id}.xlsx")
-
-    skills = {} #
-    for i in range(len(data)): #Подсчет скилов
-        for skill in data.iloc[i]['key_skills']:
-            if skill['name'] not in skills.keys():
-                skills[skill['name']] = 0
-            skills[skill['name']] += 1
-
-    skills = pd.Series(skills).sort_values(ascending=False).head(15)
-    top_skills = pd.DataFrame(skills).T
-    plot = sns.barplot(top_skills, orient='h')
-    plot.set(title=f'Top 15 skills"{message.text}"')
-    fig = plot.get_figure() # рисуем график со скиллами
-    fig.savefig(f"{message.from_id}_{message.message_id}.png", bbox_inches="tight")
-
+    manager = ProcessHhData()
+    manager.get_necessery_skills(message, vacancies)
     await message.reply(f"Обработка завершена")
     with open(f"{message.from_id}_{message.message_id}.png", "rb") as photo:
         await message.reply_photo(photo)
