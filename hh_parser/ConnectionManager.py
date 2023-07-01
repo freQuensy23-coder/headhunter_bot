f9c8413ed6429d0bd8024f5d1215aad9c4c0ce5b
freQuensy23-coder <mametevalex@gmail.com>
2023-07-02 01:56:38 +0300

    [UPD] ConnectionManager.py now support User Agent setting

--- hh_parser/ConnectionManager.py
+++ hh_parser/ConnectionManager.py
@@ -1,10 +1,18 @@
+import time
+import requests as req
+from random_user_agent.user_agent import UserAgent
+
 class ConnectionManager:
-    def __init__(self, proxies: [dict], wait_time: float = 1.0):
+    def __init__(self, proxies: [dict], wait_time: float = 1.0, user_agent_manager=None):
         """
         proxies last used - list of timestamps of usage of proxies
         :param proxies: list of proxies to rotate through
         :param wait_time: time to wait between requests
         """
+        if user_agent_manager is None:
+            self.user_agent_manager = UserAgent()
+        else:
+            self.user_agent_manager = user_agent_manager
         self.proxies = proxies
         self.proxies_last_used = [time.time()] * len(proxies)
         self.wait_time = wait_time
@@ -16,6 +24,11 @@ class ConnectionManager:
         :param kwargs: kwargs to pass to requests.get
         :return: requests.get response
         """
+        if "headers" not in kwargs:
+            kwargs["headers"] = {"User-Agent": self.user_agent_manager.get_random_user_agent()}
+        elif "User-Agent" not in kwargs["headers"]:
+            kwargs["headers"]["User-Agent"] = self.user_agent_manager.get_random_user_agent()
+
         oldest_proxy = self.oldest_proxy()
         if time.time() - self.proxies_last_used[oldest_proxy] < self.wait_time:
             print(f"Waiting for {self.wait_time - (time.time() - self.proxies_last_used[oldest_proxy])} seconds")
@@ -27,4 +40,3 @@ class ConnectionManager:
 
     def oldest_proxy(self):
         return self.proxies_last_used.index(min(self.proxies_last_used))
-
