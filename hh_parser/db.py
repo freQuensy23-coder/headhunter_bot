323efbc0cb0c6b27ed89889a82b9a24ec3c3b9c4
freQuensy23-coder <mametevalex@gmail.com>
2023-07-03 15:55:15 +0300

    [UPD] Stop program if we can't connect to DB

--- hh_parser/db.py
+++ hh_parser/db.py
@@ -6,8 +6,6 @@ uri = mongodb_database_string
 client = MongoClient(uri)
 
 # Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+
+client.admin.command('ping')
+print("Pinged your deployment. You successfully connected to MongoDB!")
